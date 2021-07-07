7996c5a05ec5819559f08275ba8154f6bea2950b
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-07-07 07:32:51 +0300

    07.07.2021 Proje Sonu

--- MvcProjeKampi/Controllers/CalenderController.cs
+++ MvcProjeKampi/Controllers/CalenderController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BussinesLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using MvcProjeKampi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +9,40 @@ using System.Web.Mvc;
 
 namespace MvcProjeKampi.Controllers
 {
-    public class CalenderController : Controller
-    {
-        // GET: Calender
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	[AllowAnonymous]
+	public class CalenderController : Controller
+	{
+		// GET: Calender
+		HeadingManager hm = new HeadingManager(new EfHeadingDal());
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View(new Calender());
+		}
+
+		public JsonResult GetEvents(DateTime start, DateTime end)
+		{
+			var viewModel = new Calender();
+			var events = new List<Calender>();
+			start = DateTime.Today.AddDays(-14);
+			end = DateTime.Today.AddDays(-14);
+
+			foreach (var item in hm.GetList())
+			{
+				events.Add(new Calender()
+				{
+					title = item.HeadingName,
+					start = item.HeadingDate,
+					end = item.HeadingDate.AddDays(-14),
+					allDay = false
+				});
+
+				start = start.AddDays(7);
+				end = end.AddDays(7);
+			}
+
+
+			return Json(events.ToArray(), JsonRequestBehavior.AllowGet);
+		}
+	}
 }
---
+++ MvcProjeKampi/Models/Calender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjeKampi.Models
+{
+    public class Calender
+    {
+        public String title { get; set; }
+
+        public DateTime start { get; set; }
+
+        public DateTime end { get; set; }
+
+        public bool allDay { get; set; }
+
+    }
+}
